4d4470b2f30093f0e8ab762ba70db215d642b2ad
Alef <alefalmawossen@gmail.com>
2024-06-13 09:35:18 -0400

    applied flex-tailwind

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Alef</div>;
+  return <div className="hidden text-sm font-semibold md:block">Alef</div>;
 }
 
 export default Username;
