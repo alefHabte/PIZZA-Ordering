b3492c2876a32b6960f0ef14d3cbb476caa06245
Alef <alefalmawossen@gmail.com>
2024-06-12 20:40:52 -0400

    added tailwind(not responsive yet)

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import Search from "../features/order/Search";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/"> Home</Link>
+    <header className="border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase">
+      <Link className="tracking-widest" to="/">
+        {" "}
+        Fast Pizza
+      </Link>
       <Search />
-      <p>Alef</p>
+      <Username />
     </header>
   );
 }
